2e59ecf39a80891f51733324ae39a1abc495562c
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2018-09-06 15:28:18 -0300

    Firebase integration

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -4,3 +4,8 @@
 -keepattributes *Annotation*
 -keepattributes Signature
 -keepattributes Exceptions
+
+##################################
+#Firebase Crashlytics
+-keep class com.crashlytics.** { *; }
+-dontwarn com.crashlytics.**
--- app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
+++ app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
@@ -1,8 +1,13 @@
 package co.lateralview.myapp.application
 
 import androidx.multidex.MultiDexApplication
+import co.lateralview.myapp.BuildConfig
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.perf.FirebasePerformance
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.squareup.leakcanary.LeakCanary
+import io.fabric.sdk.android.Fabric
 
 class MyAppApplication : MultiDexApplication() {
 
@@ -16,5 +21,19 @@ class MyAppApplication : MultiDexApplication() {
 
         LeakCanary.install(this)
         AndroidThreeTen.init(this)
+        initializeFirebase()
+    }
+
+    private fun initializeFirebase() {
+        // Initialize Firebase Crashlytics
+        if (BuildConfig.CRASHLYTICS_ENABLED) {
+            Fabric.with(this, Crashlytics())
+        }
+
+        // Initialize Firebase Performance Monitoring
+        FirebasePerformance.getInstance().isPerformanceCollectionEnabled = BuildConfig.PERFORMANCE_MONITORING_ENABLED
+
+        // Initialize Firebase Analytics
+        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(BuildConfig.ANALYTICS_ENABLED)
     }
 }
