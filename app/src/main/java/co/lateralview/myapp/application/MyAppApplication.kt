548cec69e226e281049d4301cd424fc66e4aef3b
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2018-09-06 15:04:12 -0300

    LeakCanary integration

--- app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
+++ app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
@@ -2,12 +2,19 @@ package co.lateralview.myapp.application
 
 import androidx.multidex.MultiDexApplication
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.squareup.leakcanary.LeakCanary
 
 class MyAppApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
 
+        LeakCanary.install(this)
         AndroidThreeTen.init(this)
     }
 }
