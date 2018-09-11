56bfcfcab5b8ec88ae7c6a7b1ed0903364fae329
emilioq48 <emi.quattrocchio@gmail.com>
2018-09-11 17:58:52 -0300

    Dagger2 integration

---
+++ app/src/main/java/co/lateralview/myapp/ui/activity/main/MainModule.kt
@@ -0,0 +1,13 @@
+package co.lateralview.myapp.ui.activity.main
+
+import co.lateralview.myapp.ui.util.di.ActivityScoped
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MainModule(private val activity: MainActivity) {
+
+    @Provides
+    @ActivityScoped
+    fun provideContext() = activity
+}
