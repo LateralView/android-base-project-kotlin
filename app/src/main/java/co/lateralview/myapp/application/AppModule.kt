6f7e35bba1f301b2f2a20be3a7e2249a8b6df1f4
emilioq48 <emi.quattrocchio@gmail.com>
2018-09-13 10:36:48 -0300

    * Pr comments

--- app/src/main/java/co/lateralview/myapp/application/AppModule.kt
+++ app/src/main/java/co/lateralview/myapp/application/AppModule.kt
@@ -1,6 +1,8 @@
 package co.lateralview.myapp.application
 
 import android.app.Application
+import android.content.Context
+import co.lateralview.myapp.ui.util.date.DateUtils
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -11,4 +13,11 @@ class AppModule(private val application: Application) {
     @Provides
     @Singleton
     fun providesApplication(): Application = application
+
+    @Provides
+    @Singleton
+    fun providesContext(): Context = application
+
+    @Provides
+    fun provideDateUtils(): DateUtils = DateUtils()
 }
