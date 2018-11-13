ad24792f97ae830431475e800714aa87f9217550
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2018-11-13 12:36:57 -0300

    Change flavors to build variants

--- app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
+++ app/src/main/java/co/lateralview/myapp/application/MyAppApplication.kt
@@ -3,13 +3,9 @@ package co.lateralview.myapp.application
 import androidx.multidex.MultiDexApplication
 import co.lateralview.myapp.BuildConfig
 import co.lateralview.myapp.domain.util.CrashlyticsReportingTree
-import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.perf.FirebasePerformance
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.squareup.leakcanary.LeakCanary
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class MyAppApplication : MultiDexApplication() {
@@ -31,25 +27,7 @@ class MyAppApplication : MultiDexApplication() {
         appComponent.inject(this)
         LeakCanary.install(this)
         AndroidThreeTen.init(this)
-        initializeAnalytics()
-        initializeCrashReporting()
-        initializePerformanceMonitoring()
         initializeLogs()
-    }
-
-    private fun initializeAnalytics() {
-        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(BuildConfig.ANALYTICS_ENABLED)
-    }
-
-    private fun initializeCrashReporting() {
-        // Firebase Crashlytics
-        if (BuildConfig.CRASHLYTICS_ENABLED) {
-            Fabric.with(this, Crashlytics())
-        }
-    }
-
-    private fun initializePerformanceMonitoring() {
-        FirebasePerformance.getInstance().isPerformanceCollectionEnabled = BuildConfig.PERFORMANCE_MONITORING_ENABLED
     }
 
     private fun initializeLogs() {
@@ -58,19 +36,19 @@ class MyAppApplication : MultiDexApplication() {
     }
 
     private fun initializeTimber() {
-        if (BuildConfig.LOGS_ENABLED) {
+        if (BuildConfig.DEBUG) {
             // Show logs in logcat
             Timber.plant(Timber.DebugTree())
         }
 
-        if (BuildConfig.CRASHLYTICS_ENABLED) {
+        if (BuildConfig.LOG_TO_CRASHLYTICS) {
             // Show logs and crashes in crashlytics
             Timber.plant(CrashlyticsReportingTree())
         }
     }
 
     private fun initializeStetho() {
-        if (BuildConfig.LOGS_ENABLED) {
+        if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
         }
     }
